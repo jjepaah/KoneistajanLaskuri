2e4655f21a513a13fa7770303d917cb7c8e725f3
jjepaah <jjepaah@gmail.com>
2020-02-15 20:29:47 +0200

    Koodin siistimistä

--- app/src/main/java/com/example/koneistajanlaskuri/MainActivity.java
+++ app/src/main/java/com/example/koneistajanlaskuri/MainActivity.java
@@ -7,7 +7,6 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
